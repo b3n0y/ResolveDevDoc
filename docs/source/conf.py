cf4fe55e4ff4203cd926ada661aa2dad5a5370fe
b3n0y <benoit.breault@gmail.com>
2024-04-16 12:24:14 -0400

    update readme20240401

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,11 +3,11 @@
 # -- Project information
 
 project = 'ResolveDevDoc'
-copyright = '2022, b3n0y'
+copyright = '2024, b3n0y'
 author = 'Benoit Breault'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.2.0'
 
 # -- General configuration
 
